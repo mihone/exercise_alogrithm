400ca07daa7106c3fd39b2ab6cdb2a8e067b94ec
mihone <xros1178217769@gmail.com>
2024-11-20 07:54:42 +0800

    24/11/19

--- src/main/java/com/exercise/algorithm/Main.java
+++ src/main/java/com/exercise/algorithm/Main.java
@@ -1,4 +1,4 @@
-package com.exercide.algorithm;
+package com.exercise.algorithm;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
