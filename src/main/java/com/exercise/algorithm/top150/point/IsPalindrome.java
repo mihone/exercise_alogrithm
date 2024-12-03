ce4f933a6afd142a4175363ed51f536ad2923412
mihone <xros1178217769@gmail.com>
2024-12-03 08:05:54 +0800

    24/12/2

---
+++ src/main/java/com/exercise/algorithm/top150/point/IsPalindrome.java
@@ -0,0 +1,35 @@
+package com.exercise.algorithm.top150.point;
+/**
+*  @author mihone
+*  @since 2024/11/29 8:32
+*/
+public class IsPalindrome {
+
+    public boolean isPalindrome(String s) {
+        if (s==null || s.length()==0) {
+            return true;
+        }
+        return isPalindrome(s, 0, s.length() - 1);
+
+    }
+
+    public boolean isPalindrome(String s,int start,int end) {
+        while (start < end) {
+            if (!Character.isDigit(s.charAt(start)) &&!Character.isLetter(s.charAt(start))) {
+                start++;
+                continue;
+            }
+            if (!Character.isDigit(s.charAt(end)) &&!Character.isLetter(s.charAt(end))) {
+                end--;
+                continue;
+            }
+            ;
+            if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
