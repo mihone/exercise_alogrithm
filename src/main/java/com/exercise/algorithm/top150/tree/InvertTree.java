a20cd276a73c1ed6e1e03f24100737c543c210d5
mihone <xros1178217769@gmail.com>
2024-12-23 22:15:58 +0800

    24/12/22

---
+++ src/main/java/com/exercise/algorithm/top150/tree/InvertTree.java
@@ -0,0 +1,26 @@
+package com.exercise.algorithm.top150.tree;
+
+import com.exercise.algorithm.base.TreeNode;
+
+/**
+ * 226. 翻转二叉树
+*  @author mihone
+*  @since 2024/12/22 19:49
+*/
+public class InvertTree {
+
+    public static void main(String[] args) {
+
+    }
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+
+    }
+}
