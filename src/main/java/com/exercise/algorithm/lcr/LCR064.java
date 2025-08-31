277f47e7518a22f8712edf5ca21c0c0160d12d8d
mihone <xros1178217769@gmail.com>
2025-08-31 19:02:44 +0800

    25/08/31

--- src/main/java/com/exercise/algorithm/lcr/LCR064.java
+++ src/main/java/com/exercise/algorithm/lcr/LCR064.java
@@ -8,24 +8,108 @@ package com.exercise.algorithm.lcr;
 public class LCR064 {
 
     public static void main(String[] args) {
+        String[] dict = new String[]{"hello","leetcode"};
+        MagicDictionary magicDictionary = new MagicDictionary();
+        magicDictionary.buildDict(dict);
+        System.out.println(magicDictionary.search("hello"));
+        System.out.println(magicDictionary.search("leetcode"));
+        System.out.println(magicDictionary.search("hhllo"));
+        System.out.println(magicDictionary.search("hell"));
+        System.out.println(magicDictionary.search("leetcoded"));
 
     }
-    //todo
 
-//   static class MagicDictionary {
-//
-//        /** Initialize your data structure here. */
-//        public MagicDictionary() {
-//
-//        }
-//
-//        public void buildDict(String[] dictionary) {
-//
-//        }
-//
-//        public boolean search(String searchWord) {
-//
-//        }
-//    }
+   static class MagicDictionary {
+       Trie root;
+
+    public MagicDictionary() {
+           root = new Trie();
+       }
+
+       public void buildDict(String[] dictionary) {
+           for (String word : dictionary) {
+               Trie cur = root;
+               for (int i = 0; i < word.length(); ++i) {
+                   char ch = word.charAt(i);
+                   int idx = ch - 'a';
+                   if (cur.child[idx] == null) {
+                       cur.child[idx] = new Trie();
+                   }
+                   cur = cur.child[idx];
+               }
+               cur.isFinished = true;
+           }
+       }
+
+       public boolean search(String searchWord) {
+           return dfs(searchWord, root, 0, false);
+       }
+
+       private boolean dfs(String searchWord, Trie node, int pos, boolean modified) {
+           if (pos == searchWord.length()) {
+               return modified && node.isFinished;
+           }
+           int idx = searchWord.charAt(pos) - 'a';
+           if (node.child[idx] != null) {
+               if (dfs(searchWord, node.child[idx], pos + 1, modified)) {
+                   return true;
+               }
+           }
+           if (!modified) {
+               for (int i = 0; i < 26; ++i) {
+                   if (i != idx && node.child[i] != null) {
+                       if (dfs(searchWord, node.child[i], pos + 1, true)) {
+                           return true;
+                       }
+                   }
+               }
+           }
+           return false;
+       }
+   }
+
+    static class Trie {
+        boolean isFinished;
+        Trie[] child;
+
+        public Trie() {
+            isFinished = false;
+            child = new Trie[26];
+        }
+    }
+    class MagicDictionary2 {
+        private String[] words;
+
+        public MagicDictionary2() {
+
+        }
+
+        public void buildDict(String[] dictionary) {
+            words = dictionary;
+        }
+
+        public boolean search(String searchWord) {
+            for (String word : words) {
+                if (word.length() != searchWord.length()) {
+                    continue;
+                }
+
+                int diff = 0;
+                for (int i = 0; i < word.length(); ++i) {
+                    if (word.charAt(i) != searchWord.charAt(i)) {
+                        ++diff;
+                        if (diff > 1) {
+                            break;
+                        }
+                    }
+                }
+                if (diff == 1) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
 
 }
