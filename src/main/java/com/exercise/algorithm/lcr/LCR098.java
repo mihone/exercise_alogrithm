421e833b9e17315cc39b89489f43542667e3a482
mihone <xros1178217769@gmail.com>
2025-10-05 11:07:44 +0800

    25/10/05

---
+++ src/main/java/com/exercise/algorithm/lcr/LCR098.java
@@ -0,0 +1,25 @@
+package com.exercise.algorithm.lcr;
+/**
+*  @author mihone
+*  @since 2025/10/5 10:41
+*/
+public class LCR098 {
+
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+
+    }
+}
